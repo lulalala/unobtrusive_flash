0f91d05db49363832c3e42e208edc16d025b544a
lulalala <mark@goodlife.tw>
2013-04-27 12:46:46 +0800

    Generate assets in the vendor folder if asset pipeline is supported

--- lib/generators/unobtrusive_flash/install/install_generator.rb
+++ lib/generators/unobtrusive_flash/install/install_generator.rb
@@ -6,10 +6,15 @@ module UnobtrusiveFlash
     source_root File.expand_path('../../../templates', __FILE__)
 
     def copy_files
-      empty_directory 'public/stylesheets'
-      template 'unobtrusive_flash.css', 'public/stylesheets/unobtrusive_flash.css'
-      empty_directory 'public/javascripts'
-      template 'unobtrusive_flash.js', 'public/javascripts/unobtrusive_flash.js'
+      if Rails::VERSION::STRING.starts_with?('3') && Rails.application.config.respond_to?(:assets)
+        base_folder = 'vendor/assets'
+      else
+        base_folder = 'public'
+      end
+      empty_directory "#{base_folder}/stylesheets"
+      template 'unobtrusive_flash.css', "#{base_folder}/stylesheets/unobtrusive_flash.css"
+      empty_directory "#{base_folder}/javascripts"
+      template 'unobtrusive_flash.js', "#{base_folder}/javascripts/unobtrusive_flash.js"
     end
   end
 end
